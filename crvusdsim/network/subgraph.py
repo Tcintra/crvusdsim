951113728f0ad4e6a95dac8435bea581c1ce98cf
Thomas Cintra <tncintra@gmail.com>
2023-12-22 12:12:43 -0300

    Bugfix

--- crvusdsim/network/subgraph.py
+++ crvusdsim/network/subgraph.py
@@ -589,7 +589,7 @@ async def market_snapshot(
         debt_ceilings[address] = await get_debt_ceiling(address)
 
     # fix base price
-    r["basePrice"] = str(float(r["basePrice"]) / float(r["llammaRate"]["rateMul"]))
+    r["basePrice"] = str(float(r["basePrice"]) / float(r["llammaRate"]["rateMul"]) * 1e18)
 
     # Output
     data = {
